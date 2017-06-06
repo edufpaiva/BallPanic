97311a45a416412a8d85f462e2b052dd910c6d00
EduPaiva <edufpaiva33@gmail.com>
2017-06-06 16:15:12 -0400

    - Adicionado Arrow Script

---
+++ Assets/Scripts/Arrow/ArrowScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour {
+    [SerializeField]
+    private float arrowSpeed = 4.0f;
+    private bool canShootStickyArrow;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ShootArrow();
+    }
+
+    void ShootArrow() {
+        Vector3 temp = transform.position;
+        temp.y += arrowSpeed * Time.unscaledDeltaTime;
+        transform.position = temp;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        if(col.gameObject.tag == "TopBrick") {
+            gameObject.SetActive(false);
+        }
+    }
+
+
+}
