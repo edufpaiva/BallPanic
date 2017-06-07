27888132d3eecc58fc1888d977425f33df173ee7
EduPaiva <edufpaiva33@gmail.com>
2017-06-07 18:14:23 -0400

    - adicionado a ação de atirar flechas ao player  - adicionado som ao atirar flechas  - todos os prefabs dos player foram modificados com essas alterações

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -12,16 +12,32 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     private Animator mAnimator;
 
+    [SerializeField]
+    private GameObject[] arrows;
+
+    private float heigth;
+    private bool canWalk;
+
+    [SerializeField]
+    private AnimationClip clip;
+
+    [SerializeField]
+    private AudioClip shootClip;
+
+    
+
     // Use this for initialization
     void Start() {
         playerRB = GetComponent<Rigidbody2D>();
+        heigth = -Camera.main.orthographicSize - 0.8f;
+        canWalk = true;
     }
 
     // Update is called once per frame
     void Update() {
 
+        ShootTheArrow();
 
-       
     }
 
     private void FixedUpdate() {
@@ -37,33 +53,56 @@ public class PlayerController : MonoBehaviour {
 
         float h = Input.GetAxisRaw("Horizontal");
 
-        if(h > 0) {
-            if(velocity < maxVelocity) {
-                force = speed;
+        if(canWalk) {
+            if(h > 0) {
+                if(velocity < maxVelocity) {
+                    force = speed;
+                }
+                Vector3 scale = transform.localScale;
+                scale.x = 1.0f;
+
+                transform.localScale = scale;
+
+                mAnimator.SetBool("Walk", true);
+
+            } else if(h < 0) {
+
+                if(velocity < maxVelocity) {
+                    force = -speed;
+                }
+                Vector3 scale = transform.localScale;
+                scale.x = -1.0f;
+
+                transform.localScale = scale;
+                mAnimator.SetBool("Walk", true);
+            } else {
+                mAnimator.SetBool("Walk", false);
             }
-            Vector3 scale = transform.localScale;
-            scale.x = 1.0f;
 
-            transform.localScale = scale;
-
-            mAnimator.SetBool("Walk", true);
-
-        } else if(h < 0) {
-
-            if(velocity < maxVelocity) {
-                force = -speed;
-            }
-            Vector3 scale = transform.localScale;
-            scale.x = -1.0f;
-
-            transform.localScale = scale;
-            mAnimator.SetBool("Walk", true);
-        } else {
-            mAnimator.SetBool("Walk", false);
+            playerRB.AddForce(new Vector2(force, 0));
         }
 
-        playerRB.AddForce(new Vector2(force, 0));
 
+    }
 
+    public void ShootTheArrow() {
+
+        if(Input.GetMouseButtonDown(0)) {
+            
+            StartCoroutine(PlayTheShootAnimation());
+            // USE ASSIM PARA CONSEGUIR MANIPULAR O GAMEOBJECT CRIADO DEPOIS DE INSTANCIADO
+            //GameObject arrow1 = Instantiate(arrows[0], new Vector3(transform.position.x, heigth, 0), Quaternion.identity) as GameObject;
+            Instantiate(arrows[0], new Vector3(transform.position.x, heigth, 0), Quaternion.identity);
+            
+        }
+    }
+
+    IEnumerator PlayTheShootAnimation (){
+        canWalk = false;
+        mAnimator.Play("Shoot");
+        AudioSource.PlayClipAtPoint(shootClip, transform.position);
+        yield return new WaitForSeconds(clip.length);
+        mAnimator.SetBool("Shoot", false);
+        canWalk = true;
     }
 }
